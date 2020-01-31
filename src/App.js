c82abaa541046748234d63f6440a6b381c76832b
TE-Good <tom.good1990@gmail.com>
2020-01-31 13:11:24 +0000

    no public

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'spectre.css/dist/spectre.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-container">
+        <div className="form form-group">
+          <h1>Who are you?</h1>
+          <label className="form-label">First Name</label>
+          <input className="form-input" type="text" placeholder="First Name" />
+          <label className="form-label">Surname</label>
+          <input className="form-input" type="text" placeholder="Surname" />
+          <label className="form-label">Age</label>
+          <input className="form-input" type="number" placeholder="Age" />
+          <label className="form-label">Employment Status</label>
+          <select className="form-select">
+            <option>Choose an option</option>
+            <option>Employed</option>
+            <option>Self-Employed</option>
+            <option>Unemployed</option>
+          </select>
+          <button className="btn">Submit</button>
+        </div>
+      </div>
     </div>
   );
 }
