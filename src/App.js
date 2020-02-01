e55f1f934b028e1a4021becf03e801c2beeb7e72
TE-Good <tom.good1990@gmail.com>
2020-02-01 21:42:33 +0000

    component added

--- src/App.js
+++ src/App.js
@@ -26,26 +26,8 @@ function App() {
 
   return (
     <div className="App">
-    {console.log(form)}
       <div className="form-container">
-        <div className="form form-group">
-          {/* <Form /> */}
-          <h1>Who are you?</h1>
-          <label className="form-label">First Name</label>
-          <input className="form-input disable-on-submit" type="text" name="firstName" onChange={handleChange} placeholder="First Name" />
-          <label className="form-label">Surname</label>
-          <input className="form-input disable-on-submit" type="text" name="surname" onChange={handleChange} placeholder="Surname" />
-          <label className="form-label">Age</label>
-          <input className="form-input disable-on-submit" type="number" name="age" onChange={handleChange} placeholder="Age" min="18" max="64" />
-          <label className="form-label">Employment Status</label>
-          <select className="form-select disable-on-submit" name="employmentStatus" onChange={handleChange} defaultValue="Choose an option">
-            <option disabled>Choose an option</option>
-            <option>Employed</option>
-            <option>Self-Employed</option>
-            <option>Unemployed</option>
-          </select>
-          <button className="btn" onClick={handleClick} onFocus={e => e.target.blur()}>Submit</button>
-        </div>
+        <Form handleChange={handleChange} handleClick={handleClick} />
       </div>
     </div>
   )
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-export default function Form(props) {
-  
+export default function Form({handleChange, handleClick}) {
+
   return (
-    <p>hello world</p>
+    <div className="form form-group">
+      <h1>About you</h1>
+      <label className="form-label">First Name</label>
+      <input className="form-input disable-on-submit" type="text" name="firstName" onChange={handleChange} placeholder="First Name" />
+      <label className="form-label">Surname</label>
+      <input className="form-input disable-on-submit" type="text" name="surname" onChange={handleChange} placeholder="Surname" />
+      <label className="form-label">Age</label>
+      <input className="form-input disable-on-submit" type="number" name="age" onChange={handleChange} placeholder="Age" min="18" max="64" />
+      <label className="form-label">Employment Status</label>
+      <select className="form-select disable-on-submit" name="employmentStatus" onChange={handleChange} defaultValue="Choose an option">
+        <option disabled>Choose an option</option>
+        <option>Employed</option>
+        <option>Self-Employed</option>
+        <option>Unemployed</option>
+      </select>
+      <button className="btn" onClick={handleClick} onFocus={e => e.target.blur()}>Submit</button>
+    </div>
   )
 
 }
